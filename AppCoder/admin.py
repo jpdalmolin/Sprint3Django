01db006bc839fe60da79d1c5af3195bb6f6d14b2
Juan Pablo Dal Molin <54445366+jpdalmolin@users.noreply.github.com>
2022-07-24 10:00:49 -0300

    Add files via upload

---
+++ AppCoder/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+
+admin.site.register(Familiar)
+admin.site.register(Animales)
+admin.site.register(Vehiculos)
+admin.site.register(Avatar)
+admin.site.register(Post)
